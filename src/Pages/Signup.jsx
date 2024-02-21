a9d51ac56d44d0248d11dddb4eda66cc666ade8e
dillahmwangi <mcharity241@gmail.com>
2024-02-21 11:24:24 +0300

    added more fields in the sign up form

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,47 +1,155 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Signup = () => {
+
   return (
-    <section className="bg-blueGray-50">
-    <div className="w-full lg:w-4/12 px-4 mx-auto pt-6">
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-        <div className="rounded-t mb-0 px-6 py-6">
-          <div className="text-center mb-3">
-            <h6 className="text-blueGray-500 text-sm font-bold">
-              SIGN UP
-            </h6>
-          </div>
-          <div className="btn-wrapper text-center">
-            <button className="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
-              <img alt="..." className="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/github.svg"/>Github</button>
-            <button className="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
-              <img alt="..." className="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/google.svg"/>Google </button>
-  
-          </div>
-          <hr className="mt-6 border-b-1 border-blueGray-300"/>
+
+
+    <section className="bg-Gray-500">
+     <div className="w-full max-w-3xl mx-auto py-4 px-4  xl:px-8 xl:w-6/12">  
+    <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+      <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+       REGISTER
+      </h3>
+      <form>
+       
+        <div className="mb-1 sm:mb-2">
+          <label
+            htmlFor="Full Name"
+            className="inline-block mb-1 font-medium"
+          >
+           Full Name
+          </label>
+          <input
+            placeholder="Enter your name"
+            required
+            type="text"
+            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            id="userId"
+            name="id"
+    
+          />
         </div>
-        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <div className="text-blueGray-400 text-center mb-3 font-bold">
-            <small>Or sign in with credentials</small>
-          </div>
-          <form>
-            <div className="relative w-full mb-3">
-              <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlhtmlhtmlFor="grid-password">Email</label><input type="email" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Email"/>
-            </div>
-            <div className="relative w-full mb-3">
-              <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlhtmlhtmlFor="grid-password">Password</label><input type="password" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Password"/>
-            </div>
-            <div>
-              <label className="inline-flex items-center cursor-pointer"><input id="customCheckLogin" type="checkbox" className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"/><span className="ml-2 text-sm font-semibold text-blueGray-600">Remember me</span></label>
-            </div>
-            <div className="text-center mt-6">
-              <button className="bg-gray-900 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button"> Sign Up </button>
-            </div>
-          </form>
+        <div className="mb-1 sm:mb-2">
+          <label
+            htmlFor="email"
+            className="inline-block mb-1 font-medium"
+          >
+            ID No
+          </label>
+          <input
+            placeholder="Enter your id number"
+            required
+            type="password"
+            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+          />
+        </div> 
+        <div className="mb-1 sm:mb-2">
+          <label
+            htmlFor="email"
+            className="inline-block mb-1 font-medium"
+          >
+            Email Address
+          </label>
+          <input
+            placeholder="Enter your Email Address"
+            required
+            type="password"
+            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+          />
+        </div> 
+
+        <div className="mb-1 sm:mb-2">
+          <label
+            htmlFor="email"
+            className="inline-block mb-1 font-medium"
+          >
+            Phone Number
+          </label>
+          <input
+            placeholder="Enter your Phone Number"
+            required
+            type="password"
+            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+          />
+        </div> 
+        <div className="mb-1 sm:mb-2">
+          <label
+            htmlFor="email"
+            className="inline-block mb-1 font-medium"
+          >
+            Country
+          </label>
+          <input
+            placeholder="Enter your Phone Number"
+            required
+            type="password"
+            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+          />
         </div>
-      </div>
+        <div className='flex justify-between space-x-4'>
+        <div className="mb-1 sm:mb-2">
+          <label
+            htmlFor="email"
+            className="inline-block mb-1 font-medium"
+          >
+           Password
+          </label>
+          <input
+            placeholder="Enter your Password"
+            required
+            type="password"
+            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+          />
+        </div>
+        <div className="mb-1 sm:mb-2">
+          <label
+            htmlFor="email"
+            className="inline-block mb-1 font-medium"
+          >
+            Confirm Password
+          </label>
+          <input
+            placeholder="Enter your profile"
+            required
+            type="password"
+            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+          />
+        </div>
+        </div>
+        <div className="mt-4 mb-2 sm:mb-4">
+          <Link
+          to={'/createprofile'}>
+          <button
+          
+            type="submit"
+            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-gray-900 hover:bg-gray-800 focus:shadow-outline focus:outline-none"
+          >
+            SIGN UP
+          </button>
+          </Link>
+        </div>
+     
+
+      </form>
     </div>
-  </section>
+  </div>
+  
+    </section>
   )
 }
 
