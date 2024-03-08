71ab1bba720dd74fd2c8ed24e2a3a1ce3094182d
dillahmwangi <mcharity241@gmail.com>
2024-03-08 12:15:13 +0300

    changed the navbasr flex

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ import { Link } from 'react-router-dom';
       return (
         <div className="bg-gray-900">
           <div className=" px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-            <div className="relative flex items-center justify-between">
+            <div className="relative flex items-center justify-between ">
               <Link
                 to="/"
                 aria-label="Company"
@@ -21,7 +21,7 @@ import { Link } from 'react-router-dom';
                 className="inline-flex items-center"
               >
                 <svg
-                  className="w-8 text-gray-300"
+                  className="w-8 text-gray-300 "
                   viewBox="0 0 24 24"
                   strokeLinejoin="round"
                   strokeWidth="2"
@@ -35,7 +35,7 @@ import { Link } from 'react-router-dom';
                   <rect x="14" y="1" width="7" height="6" />
                   <rect x="14" y="11" width="7" height="12" />
                 </svg>
-                <span className="ml-2 text-md font-bold tracking-wide text-gray-100 uppercase">
+                <span className="ml-2 text-md font-bold tracking-wide text-gray-100 uppercase ">
                  BorderLink Solutions
                 </span>
               </Link>
