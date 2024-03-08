1b5fefe34fecf05df2bd40afa1b0062f3f25afc4
dillahmwangi <mcharity241@gmail.com>
2024-03-08 12:15:50 +0300

    imported toast

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './hooks/useAuth.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
+    <Toaster position="top-right" toastOptions={{style:{padding:'16px', color:'#713200'}}}/>
   </React.StrictMode>,
 )
