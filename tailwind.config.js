5857b30951e5e92ec1a33ecfef962dc874c021a4
dillahmwangi <mcharity241@gmail.com>
2024-03-08 12:14:45 +0300

    added a font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily:{
+      PlayfairDisplay:['Playfair Display', 'serif'],
+    },
   },
   plugins: [],
 };
