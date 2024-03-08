b411556ceab7e52b83775b56c2ca812485f60402
dillahmwangi <mcharity241@gmail.com>
2024-03-08 12:17:07 +0300

    added code to enable user to upload a file

--- src/Pages/form.jsx
+++ src/Pages/form.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
+import useCURD from '../hooks/useCRUD';
+import toast from 'react-hot-toast';
 
 const Form = ({setOpenForm, openForm}) => {
-        const [formData, setFormData] = useState({ Name: "", Email: "" , PhoneNumber: ""});
+    const {post,get,processing} = useCURD()
+    const [formData, setFormData] = useState({
+             fileName: "",
+              document: null 
+            });
       
         const onChange = (e) =>
           setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -9,14 +15,31 @@ const Form = ({setOpenForm, openForm}) => {
         const handleClosingOverlay = () => {
             setOpenForm(false);
         };
+        const handleFileChange = (event) => {
+            const file = event.target.files[0]; // Get the first file from the array of selected files
+            setFormData((prev)=>({
+                ...prev,
+                document: file
+            }));
+          };
     
         const handleSubmit = (e) => {
             e.preventDefault();
         
-            if (!formData.Name || !formData.PhoneNumber) {
-               new Error("name and phoneNumber are required");
+            if (!formData.fileName || !formData.document) {
+               toast.error("file and filename are required");
               return;
             }
+            const payload = new FormData()
+            payload.append("fileName",formData.fileName)
+            payload.append("document",formData.document)
+
+            post("/doc/upload", payload)
+            .then((res)=>{
+                console.log("done",res)
+
+            })
+            .catch(error=>toast.error(error.error))
         };
     
       return (
@@ -30,37 +53,20 @@ const Form = ({setOpenForm, openForm}) => {
                                 absolute"> Document Name</p>
                             <input placeholder="John" type="text" className="border placeholder-gray-400 focus:outline-none
                                 focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-                                border-gray-300 rounded-md" name='Name' value={formData.Name} onChange={onChange}/>
+                                border-gray-300 rounded-md" name='fileName' value={formData.fileName} onChange={onChange}/>
                         </div>
-                        <div className="relative">
-                            <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">Date</p>
-                            <input placeholder="123@ex.com" type="text" className="border placeholder-gray-400 focus:outline-none
-                                focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-                                border-gray-300 rounded-md" name='Email' value={formData.Email} onChange={onChange}/>
-                        </div>
-                        <div className="relative">
-                            <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">
-                                Size
-                            </p>
-                            <input placeholder="07xxxxxxx" type="number" className="border placeholder-gray-400 focus:outline-none
-                                focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-                                border-gray-300 rounded-md" name='PhoneNumber' value={formData.PhoneNumber} onChange={onChange}/>
-                        </div>
-                        <div className="relative">
-                            <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">
-                               Type
-                            </p>
-                            <input placeholder="07xxxxxxx" type="number" className="border placeholder-gray-400 focus:outline-none
-                                focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-                                border-gray-300 rounded-md" name='PhoneNumber' value={formData.PhoneNumber} onChange={onChange}/>
-                        </div>
+
                         <div className="relative">
 
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900" for="file_input">Upload File</label>
-                    <input className="block w-full  text-gray-900 border focus:border-black  pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base   rounded-lg cursor-pointer bg-white dark:text-gray-400 focus:outline-none dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-900" id="file_input" type="file"/>
+                    <input className="block w-full  text-gray-900 border focus:border-black  pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base   rounded-lg cursor-pointer bg-white dark:text-gray-400 focus:outline-none dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-900"  name='file' id="file" type="file"
+                    onChange={handleFileChange}/>
                     </div>
                         <div className="relative">
-                            <button  className="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-gray-900 rounded-lg transition duration-200 hover:bg-gray-700 ease">
+                            <button 
+                            loading={processing}
+                            type="submit" 
+                             className="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-gray-900 rounded-lg transition duration-200 hover:bg-gray-700 ease">
                                 {/* {loading ? (
                                     <p className="flex justify-center items-center animate-spin">
                                         <ImSpinner9 size={30} />
