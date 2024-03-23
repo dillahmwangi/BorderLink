90bbf90d37820c9c92afd5e0b3f7d184317ec0a4
dillahmwangi <mcharity241@gmail.com>
2024-03-24 01:08:53 +0300

    changed port

--- src/hooks/axios.jsx
+++ src/hooks/axios.jsx
@@ -3,7 +3,7 @@ import { setupCache } from 'axios-cache-interceptor'
 
 export const axios = setupCache(
   Axios.create({
-    baseURL: ' http://localhost:4000/',
+    baseURL: ' http://localhost:5000/',
   })
 )
 
